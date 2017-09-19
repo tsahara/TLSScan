caa0854a81773c4062a475b38d39874d9f6c027c
Tomoyuki Sahara <sahara@caddr.net>
2017-09-19 21:06:47 +0900

    Initial Commit

---
+++ TLSScan/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TLSScan
+//
+//  Created by Tomoyuki Sahara on 2017/09/19.
+//  Copyright © 2017 Tomoyuki Sahara. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
